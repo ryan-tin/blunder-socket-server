c51199423c24e7f5dc8911069dfb720cf3a373e5
Ryan Tin <ryantin526@gmail.com>
2023-09-11 20:08:26 +0200

    Real-time lobby & game-room socket server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,72 @@
 import express from 'express';
-const app = express();
-const port = 3000;
+import http from 'http';
+import { Server } from 'socket.io';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+const app = express();
+const server = http.createServer(app)
+const port = 60001;
+const io = new Server(server, {
+  cors: {
+    origin: /http:\/\/localhost:(\d*)/, // REGEX, match all http://localhost:(any num)
+    credentials: true
+  }
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
+const DEBUG = true;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
+
+const lobbyNamespace = io.of("/lobby");
+
+lobbyNamespace.on('connection', (socket) => {
+  console.log('A user connected to lobby', socket.id);
+
+  // when a user joins a game, send it out to all others listening on the socket
+  socket.on("join game", (message) => {
+    console.log("someone joined a game:", message);
+    lobbyNamespace.emit("join game", message);
+  })
+
+  socket.on('disconnect', (reason) => {
+    console.log(`A user disconnected lobby: ${reason}`);
+  })
+});
+
+
+// use rooms for games
+const game = io.of("/game");
+game.on('connection', (socket) => {
+  if (DEBUG) {
+    console.log(`joined game ${socket.id}`)
+  }
+
+  socket.on('join room', (roomId) => {
+    if (DEBUG) {
+      console.log(`join request for room <${roomId}>`);
+    }
+    // client will remember what room it has joined
+    socket.join(roomId);
+  })
+
+  // recieving a sent move is relayed to all others in that room
+  socket.on('send move', ({ roomId, FEN }) => {
+    if (DEBUG) {
+      console.log('roomId', roomId);
+      console.log('FEN', FEN);
+    }
+    socket.to(roomId).emit('send move', FEN);
+  })
+
+  socket.on('disconnect', (reason) => {
+    if (DEBUG) {
+      console.log(`<${socket.id}> disconnected game: [${reason}]`);
+    }
+  })
+})
+
+
+server.listen(port, () => {
+  return console.log(`Express is listening at *:${port}`);
 });
